8d8b70fd331cf4ca529e4a8b872ffe6be0812f24
Sara Richardson <63122520+SaraRichardson129@users.noreply.github.com>
2020-08-13 16:23:34 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import Welcome from "./components/welcome/Welcome";
+import Clock from "./components/clock/Clock";
+import Contact from "./components/contact/Contact";
+import Navigation from "./components/navigation/Navigation";
+
+const Child = ({ match }) => (
+  <div>
+    <h3>Welcome, {match.params.name}!</h3>
+  </div>
+);
+const NoMatch = ({ location }) => (
+  <div>
+    <h3>
+      Page Not Found 404 <code>{location.pathname}</code>
+    </h3>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Switch>
+        <Route exact path="/" render={() => <Welcome name="Sara" />} />
+        <Route path="/clock" component={Clock} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/welcome/:name" component={Child} />
+        <Route component={NoMatch} />
+      </Switch>
     </div>
   );
 }
