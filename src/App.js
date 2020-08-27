aa539ffed609e4c68199e75a526f504784387270
Sara Richardson <63122520+SaraRichardson129@users.noreply.github.com>
2020-08-27 11:34:08 -0500

    fixed routing

--- src/App.js
+++ src/App.js
@@ -5,19 +5,8 @@ import Welcome from "./components/welcome/Welcome";
 import Clock from "./components/clock/Clock";
 import Contact from "./components/contact/Contact";
 import Navigation from "./components/navigation/Navigation";
-
-const Child = ({ match }) => (
-  <div>
-    <h3>Welcome, {match.params.name}!</h3>
-  </div>
-);
-const NoMatch = ({ location }) => (
-  <div>
-    <h3>
-      Page Not Found 404 <code>{location.pathname}</code>
-    </h3>
-  </div>
-);
+import Jeopardy from "./components/jeopardy/Jeopardy";
+import NoMatch from "./components/noMatch/NoMatch";
 
 function App() {
   return (
@@ -25,9 +14,15 @@ function App() {
       <Navigation />
       <Switch>
         <Route exact path="/" render={() => <Welcome name="Sara" />} />
+        <Route
+          path="/welcome/:name"
+          render={(props) => (
+            <Welcome {...props} name={props.match.params.name} />
+          )}
+        />
         <Route path="/clock" component={Clock} />
         <Route path="/contact" component={Contact} />
-        <Route path="/welcome/:name" component={Child} />
+        <Route path="/jeopardy" components={Jeopardy} />
         <Route component={NoMatch} />
       </Switch>
     </div>
---
+++ src/components/noMatch/NoMatch.js
@@ -0,0 +1,9 @@
+import React from "react";
+function NoMatch(props) {
+  return (
+    <div className="Page404">
+      <h3>Page Not Found - 404!</h3>
+    </div>
+  );
+}
+export default NoMatch;
